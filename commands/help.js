e0cf3a73f3f64d49af421ed5f18bcd5cd646d0e5
SyshCndr <48546075+SyshChan@users.noreply.github.com>
2021-03-01 19:00:24 +0530

    update

--- commands/help.js
+++ commands/help.js
@@ -8,6 +8,16 @@ const helper = () => {
 `);
 };
 
+const meow = () => {
+  console.log(`List of all the commands:
+    node mycli.js view <dir-name> flat
+    node mycli.js view <dir-name> tree
+    node mycli.js organize <dir-name>
+    node mycli.js organize 
+    node mycli.js help
+`);
+};
+
 module.exports = {
   helper: helper,
 };
