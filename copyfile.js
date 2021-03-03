7e60cdbbc350791a9e6c2d75048db3920353e553
SyshCndr <48546075+SyshChan@users.noreply.github.com>
2021-03-03 17:59:08 +0530

    add gitignore

--- copyfile.js
+++
@@ -1,7 +0,0 @@
-let fs = require("fs");
-let path = require("path");
-console.log(process.argv[2]);
-console.log(process.argv[3]);
-fs.copyFileSync(process.argv[2], path.join(process.argv[3], "f1.js"));
-console.log("file copied")
-
